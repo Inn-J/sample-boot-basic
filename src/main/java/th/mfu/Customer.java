3ae9430af2ee665ee1f5595c660b7e07819c6cd5
Inn-J <inn.javanicha@gmail.com>
2025-08-10 13:06:46 +0700

    Change Repo

---
+++ src/main/java/th/mfu/Customer.java
@@ -0,0 +1,55 @@
+package th.mfu;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+public class Customer {
+    private String name;
+    private String address;
+    private Integer id;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @JsonSerialize(using =  LocalDateSerializer.class)
+    @JsonDeserialize(using =  LocalDateDeserializer.class)
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    
+}
